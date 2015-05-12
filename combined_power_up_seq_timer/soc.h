e6d83681ca9fe3aff833258273ef7ccfcfb27a38
David Li <dhl72@github.com>
2015-05-12 12:00:59 -0400

    Added soc.h

---
+++ combined_power_up_seq_timer/soc.h
@@ -0,0 +1,22 @@
+static const int sample_size = 100;
+
+static const double ocv_soc_lookup_X[128] = {
+	
+	// fill in values
+	
+}
+
+double h[sample_size];
+
+double uf[sample_size];
+
+double vf[sample_size];
+
+double sampled_i[sample_size];
+
+double sampled_v[sample_size];
+
+int SOC;
+
+int sample_counter;
+
